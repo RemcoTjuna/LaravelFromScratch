58b851977c424110dcd55ca5f8c5317fa2f3125e
RemcoTjuna <remco@tjuna.com>
2017-09-18 11:47:16 +0200

    Worked on wrapped scopes and generated migrations based on model

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    public function isComplete(){
-        return false;
+
+    public static function completedTasks(){
+        //We are making a wrapped query within the function.
+        return static::where('completed', 0)->get();
+    }
+
+    public function scopeIsComplete($query){
+        //A Query Scope is a wrapper around a already existing/valid query.
+        return $query->where('completed', 0);
     }
 }
